cd8cdc4c60bc662fce214bfa4d6e843e5b73d774
Debasish Mandal <76836363+devDebasishMandal@users.noreply.github.com>
2024-03-05 13:08:52 +0000

    sol1

---
+++ src/components/Tabs.js
@@ -0,0 +1,27 @@
+import React,{useState} from 'react'
+
+const Tabs = ({tabs}) => {
+const [name,setName]=useState("");
+   
+function setT(e){
+// console.log();
+setName(e.target.dataset.name);
+console.log(e.target.dataset.name);
+}
+
+  return (
+    <div>
+   <ul>
+    <li data-name={tabs[0].title} onClick={setT}>{tabs[0].title}</li>
+    <li data-name={tabs[1].title} onClick={setT} >{tabs[1].title}</li>
+    <li data-name={tabs[2].title}  onClick={setT}>{tabs[2].title}</li>
+   </ul>
+
+ {name &&  <p>This is the content for {name}.</p>}
+ 
+   
+    </div>
+  )
+}
+
+export default Tabs;
