462cea6ea661ac3e283bc81ef97b1a3ffc0eeb19
Debasish Mandal <76836363+devDebasishMandal@users.noreply.github.com>
2024-03-05 13:11:14 +0000

    sol2

--- src/components/Tabs.js
+++ src/components/Tabs.js
@@ -1,7 +1,7 @@
 import React,{useState} from 'react'
 
 const Tabs = ({tabs}) => {
-const [name,setName]=useState("");
+const [name,setName]=useState("Tab 1");
    
 function setT(e){
 // console.log();
